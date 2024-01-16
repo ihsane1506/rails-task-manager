dad8429f039057e2c8842248419183b69949e4b0
ihsane ennaji ihssaneennaji29@gmail.com <ihssaneennaji29@gmail.com>
2024-01-16 17:06:54 +0100

    Done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,10 @@
+# app/controllers/tasks_controller.rb
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+end
